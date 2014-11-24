7c2fcce949a8be7a2ae02fe9f6582a1b46c367f6
Nate L <roguecode0@gmail.com>
2014-11-24 16:31:48 -0500

    Added Links

--- application/views/slices/header.php
+++ application/views/slices/header.php
@@ -19,31 +19,30 @@
 					<ul class="children">
 						<li><a href="<?php echo base_url('/inputs'); ?>"><i class="fa fa-caret-right"></i> Inputs Dashboard</a></li>
 						<li><a href="<?php echo base_url('/inputs/company_input'); ?>"><i class="fa fa-caret-right"></i> Company Input</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Bankers</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Referral Individual</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Referral Company</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> NDA Per Month</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Ellive</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> In Market LOI</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Utilization Targets</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Actuals</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> SSVSOP</a></li>
+						<li><a href="<?php echo base_url('/inputs/bankers_input'); ?>"><i class="fa fa-caret-right"></i>Bankers</a></li>
+						<li><a href="<?php echo base_url('/inputs/referral_individual_input'); ?>"><i class="fa fa-caret-right"></i>Referral Individual</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Referral Company</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>NDA Per Month</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Ellive</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>In Market LOI</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Utilization Targets</a></li>
+						<li><a href="<?php echo base_url('/inputs/actuals_input'); ?>"><i class="fa fa-caret-right"></i> Actuals</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>SSVSOP</a></li>
 					</ul>
 				</li>
 
 				<!--** Reports Section-->
 				<li class="nav-parent"><a href=""><i class="fa fa-folder-open"></i> <span>Reports</span></a>
 					<ul class="children">
-						<li><a href=""><i class="fa fa-caret-right"></i> Snapshot</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Trend Analysis LTM</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Banker Productivity</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Changes Since Last Month</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> YTD Engagements</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Live Sell-Side Engagements by Enterprise Value</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Live Engagements by Deal Slot</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Live Engagements by Status</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Revenue Estimates</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Form 5</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Snapshot</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Trend Analysis LTM</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Banker Productivity</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Changes Since Last Month</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>YTD Engagements</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Live Sell-Side Engagements by Enterprise Value</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Live Engagements by Deal Slot</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Live Engagements by Status</a></li>
+						<li><a href=""><i class="fa fa-caret-right"></i>Revenue Estimates</a></li>
 					</ul>
 				</li>
 
@@ -52,9 +51,14 @@
 						<li><a href="<?php echo base_url('/forms'); ?>"><i class="fa fa-caret-right"></i>Forms Dashboard</a></li>
 						<li><a href="<?php echo base_url('/forms/company_form'); ?>"><i class="fa fa-caret-right"></i>Company Form</a></li>
 						<li><a href="<?php echo base_url('/forms/actuals_form'); ?>"><i class="fa fa-caret-right"></i>Actuals Form</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Form 3</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Form 4</a></li>
-						<li><a href=""><i class="fa fa-caret-right"></i> Form 5</a></li>
+						<li><a href="<?php echo base_url('/forms/bankers_form'); ?>"><i class="fa fa-caret-right"></i>Bankers Form</a></li>
+						<li><a href="<?php echo base_url('/forms/ellive_form'); ?>"><i class="fa fa-caret-right"></i>Ellive Form</a></li>
+						<li><a href="<?php echo base_url('/forms/in_market_loi_form'); ?>"><i class="fa fa-caret-right"></i>Actuals Form</a></li>
+						<li><a href="<?php echo base_url('/forms/nda_per_month_form'); ?>"><i class="fa fa-caret-right"></i>NDA per Month Form</a></li>
+						<li><a href="<?php echo base_url('/forms/referral_company_form'); ?>"><i class="fa fa-caret-right"></i>Referral Company Form</a></li>
+						<li><a href="<?php echo base_url('/forms/referral_individual_form'); ?>"><i class="fa fa-caret-right"></i>Referral Individual Form</a></li>
+						<li><a href="<?php echo base_url('/forms/ssvsop_form'); ?>"><i class="fa fa-caret-right"></i>SSVSOP Form</a></li>
+						<li><a href="<?php echo base_url('/forms/utilization_targets_form'); ?>"><i class="fa fa-caret-right"></i>Utilization Targets Form</a></li>
 					</ul>
 				</li>
 			</ul>
